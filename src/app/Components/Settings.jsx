d65321fc325f44874d1223fdbdae62aa10ef171d
DevDarji16 <devdarji033@gmail.com>
2025-04-07 22:42:12 +0530

    setting field added

--- src/app/Components/Settings.jsx
+++ src/app/Components/Settings.jsx
@@ -29,7 +29,7 @@ const Settings = ({ settings,name, url,username, setSettings }) => {
 
     return (
         <div className='relative ' onClick={(e) => e.stopPropagation()}>
-            <div className={`${settings ? 'opacity-100' : 'opacity-0 pointer-events-none'} w-46   duration-300 transition-all ease-in-out rounded-lg flex flex-col ${theme === 'light' ? 'bg-[#181b1e] text-white' : 'bg-gray-100'} items-center absolute bottom-22 sm:bottom-14 left-8  shadow-lg border p-2`}>
+            <div className={`${settings ? 'opacity-100' : 'opacity-0 pointer-events-none'} w-52   duration-300 transition-all ease-in-out rounded-lg flex flex-col ${theme === 'light' ? 'bg-[#181b1e] text-white' : 'bg-gray-100'} items-center absolute bottom-22 sm:bottom-14 left-8  shadow-lg border p-2`}>
                 <div className='text-[13px] '>Settings</div>
                 <div className={`${theme==='light'?'border-gray-700':'border-gray-300'} border  w-full my-2`}></div>
                 <div className='w-full flex justify-center'> <img src={url} className='rounded-full object-cover h-12 w-12' alt="" />
@@ -37,6 +37,12 @@ const Settings = ({ settings,name, url,username, setSettings }) => {
                 <div className='font-semibold text-[14px]'>@{username}</div>
                 <div className='text-[15px]'>{name}</div>
                 <div className={`${theme==='light'?'border-gray-700':'border-gray-300'} border  w-full my-2`}></div>
+                <Link href={'/rate-us'} className='w-full'>
+                    <div className={`${theme === 'dark' ? 'hover:bg-gray-200' : 'hover:bg-[#24292e]'} text-center text-[13px] rounded-lg p-1`}>Rate us</div>
+                </Link>
+                <Link href={'/contact-us'} className='w-full'>
+                    <div className={`${theme === 'dark' ? 'hover:bg-gray-200' : 'hover:bg-[#24292e]'} text-center text-[13px] rounded-lg p-1`}>Contact us</div>
+                </Link>
                 <Link href={'/about-us'} className='w-full'>
                     <div className={`${theme === 'dark' ? 'hover:bg-gray-200' : 'hover:bg-[#24292e]'} text-center text-[13px] rounded-lg p-1`}>About us</div>
                 </Link>
